c4da9bdfacb06504841c8e88983e464a1d3f257a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:02:48 +0200

    unimportant

--- csrc/libgit2/build-osx32.sh
+++ csrc/libgit2/build-osx32.sh
@@ -1,3 +1,3 @@
 C="-arch i386 src/unix/*.c" P=osx32 \
-    L="-arch i386 -install_name @loader_path/libgit2.dylib" \
+    L="-arch i386 -install_name @rpath/libgit2.dylib" \
     D=libgit2.dylib A=libgit2.a ./build.sh
