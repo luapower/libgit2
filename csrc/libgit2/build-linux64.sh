e84d9244825b3bdb67d2df77f73c1440ac567fac
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:59:06 +0200

    unimportant

--- csrc/libgit2/build-linux64.sh
+++ csrc/libgit2/build-linux64.sh
@@ -1,1 +1,1 @@
-C="src/unix/*.c" P=linux64 L="-s -static-libgcc -lrt" D=libgit2.so A=libgit2.a ./build.sh
+C="src/unix/*.c -fPIC" P=linux64 L="-s -static-libgcc -lrt" D=libgit2.so A=libgit2.a ./build.sh
